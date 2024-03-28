bdbc975a8cb8b503083ca17f4069ed268abcd1c2
Piyushnegi04 <piyushsinghnegi1@gmail.com>
2024-03-28 16:32:07 +0530

    typecasting in python....

---
+++ typecasting.py
@@ -0,0 +1,22 @@
+#typecasting- convert  data type to another data type
+#string to int
+# x= input("Enter your name:")
+# print(x)
+
+# x = int(input("Enter your age:"))
+# print(type(x))
+# if x >18:
+#     print("eligible for voting.")
+
+#convert float to int
+# x = float(input("Enter your percentage in 12:"))
+# print(x)
+
+# x = float(input("Enter your percentage in 12:"))
+# y= int(x)
+# print(x,y)
+
+#convert int to float
+x = int(input("Enter your age:"))
+y= float(x)
+print(x,y)
